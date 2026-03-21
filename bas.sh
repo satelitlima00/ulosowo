6cbd944f5612c64b8f48d588c1fe20c8aa2c63a0
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-03-21 23:16:36 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 6 Suto-007 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 6 Suto-008 >/dev/null 2>&1 &
 sleep 10
 while true
 do
