abe4fdc771533941a335e010c893367ebfc55d94
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-03-26 16:43:58 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 6 Suto-009 >/dev/null 2>&1 &
-sleep 10
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 6 Suto-01 >/dev/null 2>&1 &
+sleep 100
 while true
 do
         echo "...ojo lali ngopi boss..."
