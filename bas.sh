88be5157044836e42708af5021a9d869b9d044cb
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-03-31 19:37:41 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Wijaya-09 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Wijaya-10 >/dev/null 2>&1 &
 sleep 100
 while true
 do
