a1ca1ac8873f74a90ae9331609b73d497d5abdb3
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-04-15 19:49:38 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 WORKER-09 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 WORKER-10 >/dev/null 2>&1 &
 sleep 100
 while true
 do
